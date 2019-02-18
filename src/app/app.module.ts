864f43b1df3e622be4d5705dd1e6f582fcc96a11
Evan Armbrister <evananthony13@gmail.com>
2019-02-18 12:32:20 -0500

    Added Counter State example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { counterReducer } from './counter.reducer';
+import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot(reducers, { metaReducers })
+    StoreModule.forRoot({ count: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
